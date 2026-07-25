011587a563d89b3776f59455873dcc5ea0be44cc
plusmaloma-dev <plusmaloma@gmail.com>
2026-07-25 18:55:48 +0300

    feat: export Standard bot orchestrator

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ export * from './gameplay/bot/BotObservationService.js';
 export * from './gameplay/bot/StandardCardPolicy.js';
 export * from './gameplay/bot/HandStrengthEvaluator.js';
 export * from './gameplay/bot/StandardBidPolicy.js';
+export * from './gameplay/bot/StandardBotPolicy.js';
 export * from './persistence/types.js';
 export * from './persistence/InMemoryScoreSheetRepository.js';
 export * from './persistence/LocalStorageScoreSheetRepository.js';
